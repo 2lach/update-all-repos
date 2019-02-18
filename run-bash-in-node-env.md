65b59c43f3fa698b2895ce76709a036c2ec97b38
Stefan <stefanlachmann@hotmail.com>
2019-02-18 22:51:19 +0100

    change-pkg-name

--- run-bash-in-node-env.md
+++ run-bash-in-node-env.md
@@ -1,4 +1,48 @@
+<!-- markdownlint-disable -->
+
 Dec 17, 2016
 Running Bash scripts from Node.js
 How simple Bash scripts can add some old-school spice (and serious power) to your Node projects
 https://medium.com/the-readme/old-school-bash-for-your-node-projects-dfc8ed674a91
+
+
+https://nodejs.org/api/child_process.html
+
+
+
+this guy:
+https://tutorialedge.net/javascript/nodejs/executing-shell-scripts-with-nodejs/
+
+
+
+link bin -> 
+https://www.npmjs.com/package/link-bin
+
+To test it you can also run npm install -g . in the folder where you added the above app.js and package.json files.
+
+
+
+check out npm struct from https://github.com/sindresorhus/trash-cli/blob/master/cli.js
+
+
+--------------------------------------------------------------------------------------
+// const spawn = require('child_process').spawn();
+// const exec = require("child_process").exec(); //command[, options][, callback])
+// const exec = require('child_process').exec;
+// const { exec } = require('child_process');
+
+// spawn('sh', [''], { cwd: 'bash ./get-all-updates.sh' })
+
+// exec('"/path/to/test file/test.sh" arg1 arg2');
+// Double quotes are used so that the space in the path is not interpreted as
+// multiple arguments
+/*
+exec('sh ./../get-all-updates.sh', (error, stdout, stderr) => {
+if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
+*/
---
+++ yes/get-all-updates.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+NC='\033[0m'                    # No Color
+Red='\033[0;31m'                # Red
+Black='\033[0;30m'              # Black    
+DarkGray='\033[1;30m'           # DarkGray   
+LightRed='\033[1;31m'           # LightRed   
+Green='\033[0;32m'              # Green      
+LightGreen='\033[1;32m'         # LightGreen 
+BrownOrange='\033[0;33m'        # BrownOrange
+Yellow='\033[1;33m'             # Yellow     
+Blue='\033[0;34m'               # Blue       
+LightBlue='\033[1;34m'          # LightBlue  
+Purple='\033[0;35m'             # Purple     
+LightPurple='\033[1;35m'        # LightPurple
+Cyan='\033[0;36m'               # Cyan       
+LightCyan='\033[1;36m'          # LightCyan  
+LightGray='\033[0;37m'          # LightGray  
+White='\033[1;37m'              # White 
+
+folders=(`ls`)
+
+
+len=${#folders[*]}
+
+i=0
+while [ $i -lt $len ]; do
+echo "$i: ${folders[$i]}"
+
+F_NAME="${folders[$i]}"
+
+if test -d "${F_NAME}"
+then
+echo -e "${Cyan}Stepping into: ${F_NAME}/"
+    echo " "
+    cd $F_NAME
+    if [ -d ".git/" ]; then 
+        echo -e "${LightGreen}Git repo found"
+        BRANCH=$(git rev-parse --abbrev-ref HEAD)
+        if [[ "$BRANCH" == "master" ]]; then
+        echo -e "${LightGreen}The current bransch is master${NC}"
+
+        echo -e "${BrownOrange}Doing a git pull${NC}"
+        git pull origin master
+        echo -e "${LightGreen}Completed"
+        echo " "
+        fi
+    elif [[ "$BRANCH" != "master" ]]; then
+        echo -e "${LightPurple}Branch not master${NC}"
+        echo " "
+    fi
+    cd ..
+    echo -e "${Cyan}Left directory: ${F_NAME} ${NC}"
+    echo " "
+
+else                                   
+echo -e "found ${F_NAME} doing nada..."
+echo " "
+fi
+
+let i++
+done
+echo -e "All repo in: ${folders} have been updated"
+
